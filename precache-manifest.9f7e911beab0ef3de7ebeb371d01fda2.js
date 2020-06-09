e3e8192a132cd2fb84b9437fb07e03041d83b90a
adamhanna <adamhannadev@gmail.com>
2020-06-09 14:54:18 -0700

    Updates

---
+++ precache-manifest.9f7e911beab0ef3de7ebeb371d01fda2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa55792a86e943bd9fb8d87d83669ff3",
+    "url": "/quartet/index.html"
+  },
+  {
+    "revision": "0dfa5f5ea312fe23d02b",
+    "url": "/quartet/static/css/main.d9d6bad0.chunk.css"
+  },
+  {
+    "revision": "635e97b402a591d5752e",
+    "url": "/quartet/static/js/2.09718248.chunk.js"
+  },
+  {
+    "revision": "fbfb5e42a41e7004782949e93fdf296a",
+    "url": "/quartet/static/js/2.09718248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dfa5f5ea312fe23d02b",
+    "url": "/quartet/static/js/main.e11a4beb.chunk.js"
+  },
+  {
+    "revision": "c9c13b636ba9c63371b2",
+    "url": "/quartet/static/js/runtime-main.dd1b6d7b.js"
+  }
+]);
